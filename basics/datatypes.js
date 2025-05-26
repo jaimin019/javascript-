d136cc8a00f6129a60c35d365ca841dd1ec7fa77
jaimin019 <jaiminhadvani009@gmail.com>
2025-05-26 11:28:02 +0000

    lecture 5

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // will treat all JS code as newer version
+
+// alert( 19 + 8 + 5) // doesnt works here because we are using nodejs and not browser
+
+let jam = 999
+let fam = "family"
+let bam = true
+
+let sam;
+
+// number  -> 2^53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value // OBJECT
+// undefined -> undefined
+// symbol -> unique
+
+console.table([typeof "Jaimin", typeof 69])
+
+console.table([typeof null, typeof undefined])
