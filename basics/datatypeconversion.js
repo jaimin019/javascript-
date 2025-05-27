e627ce89186917d4cb8eca03392df87661adadd3
jaimin019 <jaiminhadvani009@gmail.com>
2025-05-27 06:58:46 +0000

    till lecture 10

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,49 @@
+// let num = 555
+
+// console.log(typeof num)
+// console.log(num)
+
+// let numstr= String(num)
+
+// console.log(typeof numstr)
+// console.log(numstr)
+
+// let str = "JA5"
+
+// let strnum = Number(str)
+
+// console.log(typeof strnum)
+// console.log(strnum) // NAN // not a number
+
+// let vari = null
+
+// let varii = undefined 
+
+// let varinum = Number(vari)
+// let variinum = Number(varii)
+
+// console.log(typeof varinum)
+// console.log(typeof variinum)
+
+// let boolstr = Boolean(str)
+
+// console.log(boolstr)
+
+
+//Operations _ Tricky
+
+
+console.log(1 + 2)
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1 + 2 + 2)
+console.log(1 + 2 + "2")
+console.log("1" + 2 + 2)
+
+console.log("Jai" + "mon" + " Bhai")
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
